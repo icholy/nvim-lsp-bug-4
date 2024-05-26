a0f570bbabf2b9b2e11a74970db72f545f152aec
Ilia Choly <ilia.choly@gmail.com>
2024-05-26 14:04:57 -0400

    stop using util

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 local winid
 
 vim.api.nvim_create_autocmd("FileType", {
@@ -8,12 +7,28 @@ vim.api.nvim_create_autocmd("FileType", {
 			name = "marksman",
 			cmd = { "marksman" },
 		})
-		vim.schedule(function ()
+		vim.schedule(function()
 			vim.api.nvim_win_close(winid, true)
 		end)
 	end
 })
 
 function repro()
-	_, winid = vim.lsp.util.open_floating_preview({"hello"}, "markdown")
+	-- Create the buffer
+	local bufnr = vim.api.nvim_create_buf(false, true)
+
+	-- Set up the contents, using treesitter for markdown
+	vim.bo[bufnr].filetype = 'markdown'
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "" })
+
+	winid = vim.api.nvim_open_win(bufnr, false, {
+		col = 0,
+		height = 1,
+		relative = "cursor",
+		row = 1,
+		width = 1,
+	})
+
+	-- disable folding
+	vim.bo[bufnr].bufhidden = 'wipe'
 end
